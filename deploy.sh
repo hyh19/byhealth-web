68e5d5f2c9e40f5ebc2d2203ae0288a0def99e19
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-07-10 11:36:33 +0800

    *修改部署脚本

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-# 源码包的路径
+# 执行脚本的参数是前端源码包的路径
 TAR_FILE=${1}
 
-# 判断有没有源码包路径作为参数
+# 判断有没有传参数
 if [ -z ${TAR_FILE} ]
 then
+	echo "[ERROR] missing parameter"
 	echo "[USAGE] deploy.sh /path/to/tar/file"
 	exit 1
 fi
@@ -13,23 +14,30 @@ fi
 # 判断源码包是否存在
 if [ ! -e ${TAR_FILE} ]
 then
-	echo "[ERROR] file is not exist."
+	echo "[ERROR] tar file is not exist."
 	exit 1
 fi
 
-# build文件夹，源码放置的位置。
+# build文件夹，解压源码包存放的位置。
 BUILD_DIR=./build
 
-# 清空原来的代码
+# 如果旧的代码存在，则先清空，然后再重新解压。
 if [ -d ${BUILD_DIR} ]
 then
 	rm -rf ${BUILD_DIR}
 fi
 
+# Dockerfile的路径
+DOCKER_FILE=./Dockerfile
+
+# .dockerignore的路径
+DOCKER_IGNORE_FILE=./.dockerignore
+
 # 重新解压代码
 tar xvf ${TAR_FILE}
 
-cp Dockerfile .dockerignore build
+# 拷贝Dockerfile和.dockerignore到build文件夹
+cp ${DOCKER_FILE} ${DOCKER_IGNORE_FILE} ${BUILD_DIR}
 
 # 开始部署
 docker-compose up
